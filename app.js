009e938d2b23a83ca4cacbd6b564ea0790a45dac
Divyashakti Pal <divyashaktipal@gmail.com>
2025-10-17 00:02:07 +0530

    initilize repo

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRouter = require("./routes/user.routes");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectToDB = require("./config/db");
+connectToDB();
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRouter);
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
